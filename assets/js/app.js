241935ff56d729621ea82119fa57c6ff4e7a66e6
jmrg <jmrg2327@gmail.com>
2025-07-07 19:01:48 -0600

    se agregaron comentarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('.name'); 
+const $n = document.querySelector('.name'); //agregamos el punto
 const $b = document.querySelector('.blog'); 
-
 const $l = document.querySelector(`.location`);
 
 async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  const data = await response.json();
+  const response = await fetch(`${usersEndpoint}/${username}`); //agregamos backticks
+  const data = await response.json(); //respuesta a JSON
   console.log(data);
-  $n.textContent = `${data.name}`;
-  $b.textContent = `${data.blog}`;
-  $l.textContent = `${data.location}`;
+  $n.textContent = `${data.name}`; // backticks
+  $b.textContent = `${data.blog}`; // bakcticks
+  $l.textContent = `${data.location}`; // backticks
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  $n.textContent = `Algo salió mal: ${err}`;
+  $n.textContent = `Algo salió mal: ${err}`; //agregamos signo $ a la variable y backticks en algo salió mal
 }
-
-displayUser('stolinski').catch(handleError);
+// también aquí faltaba un ) al final de handle error 
+displayUser('stolinski').catch(handleError); 
